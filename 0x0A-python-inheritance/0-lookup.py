724c5b3afb33f71d5a16f0786481810f1ef33f6c
Rahma <Rahma@gmail.com>
2023-09-11 19:20:53 -0700

    intarite

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-'''lookup method.'''
+'''Module for lookup method.'''
+
 
 def lookup(obj):
-    """Return a list of an object's available attributes."""
+    '''Looks up object attributes and methods.
+    Args:
+        obj (object): the object to list.
+
+    Returns:
+        list: the list of attributes.
+    '''
     return dir(obj)
